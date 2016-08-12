0b3e9890fff4b267cd10ee7a2fbbf2ff71521cf5
Stirzaker <Nigel.Stirzaker@disney.com>
2016-08-13 00:03:22 +0100

    more logging

--- www/js/camera.js
+++ www/js/camera.js
@@ -8,7 +8,8 @@ $(document).on('pageinit', function () {
 })
 
 function onPhotoDataSuccess(theImagesFileURI) {
-
+  $('#textarea').val('onPhotoDataSuccess');
+  $('#textarea').val(theImagesFileURI);
   $("#image").attr("src", theImagesFileURI);
   $("#image").css("display", 'block');
 
@@ -19,13 +20,13 @@ function onFail(message) {
 }
 
 function capturePhoto() {
-  $('#textarea').val('capture Photo');
+  $('#textarea').val('capturePhoto');
   // Take picture using device camera and retrieve image as base64-encoded string
 
   navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
     quality: 50,
     destinationType: Camera.DestinationType.FILE_URI,
-    sourceType : Camera.PictureSourceType.CAMERA
+    sourceType: Camera.PictureSourceType.CAMERA
   });
 
 }
